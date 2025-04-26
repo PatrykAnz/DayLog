b12b135ac0d1cae4ccd8b650dedc1b4a49c944a1
PatrykAnz <patrykabusiness@gmail.com>
2025-04-26 22:27:38 +0200

    fixed geolocation user_data saving

--- api/geolocation.py
+++ api/geolocation.py
@@ -7,25 +7,21 @@ choices = {
     2: "Using IP (less accurate)",
 }
 
-def create_user_data(data_folder, data_file):
-    if not data_folder.exists():
-        data_folder.mkdir()
-        print("Created 'user_data' folder.")
-    
-    if not data_file.exists():
-        with open(data_file, "w") as f:
-            json.dump({}, f)
-        print("Created 'user_data.json' file.")
 
-def geolocation():
+def get_geolocation():
     data_folder = Path("user_data")
     data_file = data_folder / "user_data.json"
-    create_user_data(data_folder, data_file)
     
     print("Choose if you want to get it from city name or IP")
     for key, value in choices.items():
         print(f"{key}: {value}")
     user_choice = int(input(""))
+    
+    try:
+        with open(data_file, "r") as f:
+            existing_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = {}
     
     if user_choice == 1:
         city_name = input("Provide the name of the city: ")
@@ -34,8 +30,14 @@ def geolocation():
             print(f"Location set to {g.address}:")
             print(f"Latitude: {g.latlng[0]}, Longitude: {g.latlng[1]}")
             
+            existing_data["Geolocation"] = {
+                "latitude": g.latlng[0], 
+                "longitude": g.latlng[1], 
+                "address": g.address
+            }
+            
             with open(data_file, "w") as f:
-                json.dump({"Geolocation": {"latitude": g.latlng[0], "longitude": g.latlng[1], "address": g.address}}, f, indent=4)
+                json.dump(existing_data, f, indent=4)
                 print(f"Saved location data to {data_file}")
             return g.latlng
         else:
@@ -46,8 +48,15 @@ def geolocation():
         if g.latlng:
             print(f"Location set to {g.city}:")
             print(f"Latitude: {g.latlng[0]}, Longitude: {g.latlng[1]}")
+            
+            existing_data["Geolocation"] = {
+                "latitude": g.latlng[0], 
+                "longitude": g.latlng[1], 
+                "address": g.address
+            }
+            
             with open(data_file, "w") as f:
-                json.dump({"Geolocation": {"latitude": g.latlng[0], "longitude": g.latlng[1], "address": g.address}}, f, indent=4)
+                json.dump(existing_data, f, indent=4)
                 print(f"Saved location data to {data_file}")
             return g.latlng
         else:
@@ -55,4 +64,4 @@ def geolocation():
             return None
 
 if __name__ == "__main__":
-    geolocation()
+    get_geolocation()
