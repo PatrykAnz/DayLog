54c1211fb76f5b26cf81eb69fdd2deb82fe85f30
PatrykAnz <patrykabusiness@gmail.com>
2025-05-08 01:12:10 +0200

    added User_data_operations function to utils for easier checking

---
+++ utils/user_data_operations.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import json
+
+data_folder = Path("user_data")
+data_file = data_folder / "user_data.json"
+
+
+def check_and_create_user_data():
+    if not data_folder.exists():
+        data_folder.mkdir()
+        print("Created 'user_data' folder.")
+
+    if not data_file.exists():
+        with open(data_file, "w") as f:
+            json.dump({}, f)
+        print("Created 'user_data.json' file.")
+    print("CHECK")
