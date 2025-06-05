3e8ba3ec1057b70143aaeac92b9f81cac055d1cc
PatrykAnz <patrykabusiness@gmail.com>
2025-06-05 17:19:48 +0200

    Changed the language to english

--- backend/core/workouts.py
+++ backend/core/workouts.py
@@ -15,36 +15,36 @@ def get_workout():
 
 
 def create_workout():
-    workout_name = input("Podaj nazwę treningu (np. Plecy Pull):\n").strip()
+    workout_name = input("Enter workout name (e.g., Back Pull):\n").strip()
     while True:
         try:
             amount_of_exercises = int(
-                input("Podaj ilość ćwiczeń w danym dniu (np. 5):\n")
+                input("Enter number of exercises for this day (e.g., 5):\n")
             )
             break
         except ValueError:
-            print("Podaj liczbę całkowitą.")
+            print("Please enter a whole number.")
 
     exercises = []
 
     for i in range(1, amount_of_exercises + 1):
-        name = input(f"Nazwa ćwiczenia {i}:\n").strip()
+        name = input(f"Exercise {i} name:\n").strip()
 
         while True:
             try:
-                sets = int(input("Ile serii?\n"))
+                sets = int(input("How many sets?\n"))
                 break
             except ValueError:
-                print("Podaj liczbę całkowitą.")
+                print("Please enter a whole number.")
 
         sets_data = []
         for j in range(1, sets + 1):
             while True:
                 try:
-                    reps = int(input(f"Powtórzenia w serii {j}:\n"))
+                    reps = int(input(f"Repetitions in set {j}:\n"))
                     break
                 except ValueError:
-                    print("Podaj liczbę całkowitą.")
+                    print("Please enter a whole number.")
             sets_data.append({"set": j, "reps": reps})
 
         exercises.append({"exercise": name, "sets": sets_data})
