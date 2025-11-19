a7df1b1cd399d34aecf1f3d769df4137ee5abce0
PatrykAnz <patrykabusiness@gmail.com>
2025-11-19 01:53:24 +0100

    fix/ database indentation

--- database/database.py
+++ database/database.py
@@ -9,40 +9,40 @@ def init_db():
     db_host = os.environ.get("DB_HOST", "daylog-db-cnpg-rw.daylog.svc.cluster.local")
     db_port = get_secret("daylog-db-port")
     
-        conn = psycopg2.connect(
+    conn = psycopg2.connect(
         dbname=db_name,
         user=db_user,
         password=db_password,
         host=db_host,
         port=db_port
+    )
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS garmin (
+            date DATE PRIMARY KEY,
+            total_steps INT,
+            total_sleep_seconds INT,
+            deep_sleep_seconds INT,
+            light_sleep_seconds INT,
+            rem_sleep_seconds INT,
+            awake_sleep_seconds INT,
+            min_hr INT,
+            rest_hr INT,
+            last7_avg_rest_hr INT,
+            avg_stress INT,
+            stress_duration INT,
+            avg_spo2 FLOAT,
+            min_spo2 FLOAT,
+            last7_avg_spo2 FLOAT,
+            avg_sleep_resp FLOAT,
+            avg_waking_resp FLOAT,
+            body_battery_most_recent INT,
+            body_battery_at_wake INT,
+            body_battery_lowest INT
         )
-        cur = conn.cursor()
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS garmin (
-                date DATE PRIMARY KEY,
-                total_steps INT,
-                total_sleep_seconds INT,
-                deep_sleep_seconds INT,
-                light_sleep_seconds INT,
-                rem_sleep_seconds INT,
-                awake_sleep_seconds INT,
-                min_hr INT,
-                rest_hr INT,
-                last7_avg_rest_hr INT,
-                avg_stress INT,
-                stress_duration INT,
-                avg_spo2 FLOAT,
-                min_spo2 FLOAT,
-                last7_avg_spo2 FLOAT,
-                avg_sleep_resp FLOAT,
-                avg_waking_resp FLOAT,
-                body_battery_most_recent INT,
-                body_battery_at_wake INT,
-                body_battery_lowest INT
-            )
-        """)
-        conn.commit()
-        return conn, cur
+    """)
+    conn.commit()
+    return conn, cur
 
 
 def insert_data(cur, conn, data):
